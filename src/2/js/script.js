1f4df7760c5692e18b47c1fc6dad81245c211d71
Heroin-lab <vip.serkin@gmail.com>
2021-04-03 12:45:16 +0300

    Correct json read

--- src/2/js/script.js
+++ src/2/js/script.js
@@ -1,10 +1,27 @@
-var requestURL = 'http://localhost:8080/question';
-
+var requestURL = 'http://localhost:8080/';
 var xhr = new XMLHttpRequest();
+var result = [];
 
-xhr.open('GET', requestURL);
 
-xhr.onload = function() {
-    console.log(JSON.parse(xhr.response));
+var getReq = function(url) {
+    var result;
+    xhr.open('GET', requestURL + url);
+    xhr.onload = function() {
+        result = JSON.parse(xhr.response);
+        check(result);
+    }
+    xhr.send();
 }
-xhr.send();
+
+var check = function(arrObjects) {
+    for (var i = 0; i < arrObjects.length; i++){
+        var pattern = document.getElementById('sample').content;
+        var copyHTML = document.importNode(pattern, true);
+        copyHTML.querySelector(".project__title").textContent = arrObjects[i].theme;
+        copyHTML.querySelector(".project__text").textContent = arrObjects[i].quesText;
+        copyHTML.querySelector(".project__answer").textContent = arrObjects[i].correctAnsw;
+        document.querySelector(".project").appendChild(copyHTML);
+    } 
+}
+
+getReq('question');
